262911576ca718042774d2380793c7c3c9465421
Jérémie Boulay <jeremi.boulay@gmail.com>
2018-10-29 20:47:20 -0400

    :gem: mobile integration

--- src/index.js
+++ src/index.js
@@ -1,4 +1,9 @@
-const projectPart =  document.getElementById('projects-part');
+import { hasTouch } from './util/';
+
+if (!hasTouch()) document.body.classList.add('no-touch');
+
+// DEV UTILS
+const projectPart = document.getElementById('projects-part');
 const aboutPart = document.getElementById('about-part');
 
 function toggle() {
---
+++ src/util/index.js
@@ -0,0 +1,6 @@
+
+// https://stackoverflow.com/questions/23885255/how-to-remove-ignore-hover-css-style-on-touch-devices
+export const hasTouch = () => 'ontouchstart' in document.documentElement ||
+  navigator.maxTouchPoints > 0 ||
+  navigator.msMaxTouchPoints > 0
+;
